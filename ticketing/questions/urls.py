13fbcc0b7cfe8bc8a8921699c37fd51b6a6b445c
robert-jan <lub@countcooper.nl>
2019-04-10 21:51:29 +0800

    creats question form, makes inital view and template pending questions.

--- ticketing/questions/urls.py
+++ ticketing/questions/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
+app_name = "questions"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("questions.urls"))
+    path('', views.pending_questions, name="pending_questions")
 ]
--- ticketing/questions/views.py
+++ ticketing/questions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import QuestionForm
+from .models import Question
+
 
 # Create your views here.
+
+def pending_questions(request):
+    context = {}
+    form = QuestionForm
+    questions_pending = Question.objects.filter(status=False)
+    questions_completed = Question.objects.filter(status=True).order_by("date_answered")[:10]
+
+
+    context["form"] = form
+    context["questions_pending"] = questions_pending
+    context["questions_completed"] = questions_completed
+
+    return render(request, "pending_questions.html", context=context)
