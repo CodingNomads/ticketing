38b8b1be19d1a47ac08186172af5e5cb691c80d0
Mingyyy <j.yanming@gmail.com>
2019-04-10 23:09:21 +0800

    Style completed.html

--- ticketing/questions/views.py
+++ ticketing/questions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Question
 from django.contrib.auth.decorators import login_required
 
+
 def completed(request):
     questions = Question.objects.filter(status=True)
     context = {"questions": questions}
