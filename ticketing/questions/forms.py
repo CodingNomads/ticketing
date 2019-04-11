9042bf0e0062d03fe32fe5cec8d4ffb5c4cd45be
robert-jan <lub@countcooper.nl>
2019-04-11 17:16:50 +0800

    apply crispy forms

--- ticketing/questions/forms.py
+++ ticketing/questions/forms.py
@@ -6,3 +6,11 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ["title", "description"]
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': "100%", 'rows': 5}),
+            'title': forms.TextInput(attrs={"cols": "100%"})
+        }
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields["description"].widget.attrs.update({'type': 'textarea;'})
