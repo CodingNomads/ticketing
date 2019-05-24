3912a7b941ea96e54fbe8c1b238facb9af1ba693
martin-martin <breuss.martin@gmail.com>
2019-05-24 23:07:42 +0930

    add lmgtfy button

--- ticketing/ticketing/settings.py
+++ ticketing/ticketing/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_FILE_DIRS = [
+
+]
 
 AUTH_USER_MODEL = 'users.User'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
